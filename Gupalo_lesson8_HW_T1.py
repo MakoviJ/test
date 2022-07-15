dd9f55e83676cb832e7d40c0ced94b94779f8ed8
Rostyslav <drop.gry@gmail.com>
2022-07-15 18:42:04 +0300

    hello

---
+++ Gupalo_lesson8_HW_T1.py
@@ -0,0 +1,10 @@
+# Task 1
+# A simple function.
+#
+# Create a simple function called favorite_movie, which takes a string containing the name of your
+# favorite movie. The function should then print “My favorite movie is named {name}”.
+
+def favourite_movie(name):
+    print(f'My favourite movie is named {name}')
+
+favourite_movie(input('Please enter a name of movie: '))
